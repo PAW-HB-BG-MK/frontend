bdef17fbcb0f866bd37855d1be4d3f79988d81f1
Arrlen <46503202+Arrlen@users.noreply.github.com>
2020-12-03 11:25:43 +0100

    Add table form (#14)
    * add mock view for adding Tables
    
    * fixes in coding style

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import DashboardT from "./screens/dashboard/DashboardT"
 import Login from './screens/Login'
 import Logout from './screens/Logout'
 import NavBar from "./components/NavBar"
+import AddTable from "./screens/dashboard/AddTable";
 import ChangeTableName from "./screens/dashboard/ChangeTableName";
 import Register from "./screens/Register";
 
@@ -18,9 +19,10 @@ function App() {
     return (
         <Router>
             <NavBar/>
-
             <Switch>
                 <Route exact path="/dashboard/tables" component={DashboardT}/>
+                <Route path="/dashboard/addtable">
+                    <AddTable/>
                 <Route path="/changetablename">
                     <ChangeTableName/>
                 </Route>
